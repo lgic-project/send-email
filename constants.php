d0a763741643533a343614e5ded49e07309c1a66
Sunil Sapkota <suneelsapkota9@gmail.com>
2024-12-20 15:50:35 +0545

    feat: add required constant data

---
+++ constants.php
@@ -0,0 +1,7 @@
+<?php
+define('SMTP_HOST', 'smtp.gmail.com');
+define('SMTP_USERNAME', 'email@gmail.com');
+define('SMTP_PASSWORD', '1234 abcd 2134 abcd'); // Use a secure app password, not your main email password go to https://myaccount.google.com/u/2/apppasswords this like to create app password. Don't use regular password here. and make sure you add 2 factor authentication.
+define('FROM_EMAIL', 'sender@gmail.com');  // Sender email address
+define('RECIPIENT_EMAIL', 'reciver@example.com'); // Recipient email address
+?>
